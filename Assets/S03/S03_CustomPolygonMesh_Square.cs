50446dbe0e8237cee191bbac75adfc956d10f0d8
kryeon <kry1105@ewha.ac.kr>
2026-09-10 00:43:46 +0900

    Update S03_CustomPolygonMesh_Square.cs
    과제 S03 두 번째 업데이트

--- Assets/S03/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -16,12 +16,11 @@ public class S03_CustomPolygonMesh_Pentagon : MonoBehaviour
         };
 
         // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
-        int[] triangles = new int[4]
+        int[] triangles = new int[]
         {
-              // 예: 0, 1, 2,
-              0, 1, 2,
-              0, 2, 3,
-
+            0, 1, 2,
+            1, 2, 3,
+            2, 3, 4,
         };
 
         Mesh mesh = new Mesh();

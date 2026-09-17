8a2f2041b401b89e63bd58d67faf47d3d3a1f971
kryeon <kry1105@ewha.ac.kr>
2026-09-17 14:30:48 +0900

    S04 네 번째 커밋(완)
    다이아몬드 만들기 완료

--- Assets/S04/S04_CustomDiamondMesh.cs
+++ Assets/S04/S04_CustomDiamondMesh.cs
@@ -28,6 +28,8 @@ public class S04_CustomDiamondMesh : MonoBehaviour
             // 아래쪽 4면 (정점 5 사용)
             5,0,1,
             5,1,2,
+            5,2,3,
+            5,3,0
         };
 
         Mesh mesh = new Mesh();
